3734c7a71bc64bb5fb181497ecf2b4777d884742
Zhanghuaxiao <1206136081@qq.com>
2020-12-02 15:01:33 +0800

    react-router

---
+++ src/Pages/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  <h2>主内容</h2> );
+    }
+}
+
+export default Index;
